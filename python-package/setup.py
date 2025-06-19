23ea5ed4dc8f2a05f6f8265233266b205657d3c1
LucWill <luc36@posteo.de>
2025-06-19 16:03:20 +0200

    lr_model file

--- python-package/setup.py
+++ python-package/setup.py
@@ -6,15 +6,17 @@ requirements = (cwd / 'fashion_pipeline' /
                 'requirements.txt').read_text().split('\n')
 
 
-
 setup(
-    name='fashion_pipeline',  # 🔁 Change this to your package name
+    name='fashion_pipeline',
     version='0.1.0',
-    description='A machine learning pipeline for product review classification.',
+    description="""A machine learning pipeline
+        for product review classification.""",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    package_data={'': ['./data/reviews.csv', './data/rf_model.pkl', 'requirements.txt']},
+    package_data={'': ['./data/reviews.csv',
+                       './data/gb_model.pkl',
+                       './data/lr_model.pkl',
+                       'requirements.txt']},
     install_requirements=requirements,
 )
-
